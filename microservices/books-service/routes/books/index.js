1f6196ac5bc89be4af9ec619de5865bb11a0ba50
William <lopezvasquezwillianalexander@gmail.com>
2023-04-20 18:17:08 -0600

    Primer commit

---
+++ microservices/books-service/routes/books/index.js
@@ -0,0 +1,186 @@
+const express = require("express"); // importa Express
+const router = express.Router(); // crea un nuevo enrutador de Express
+const data = require("../../data/data-library"); // importa los datos de data-library
+
+const logger = (message) => console.log(`Author Services: ${message}`);
+
+// define un controlador para la ruta raíz ("/")
+router.get("/", (req, res) => {
+  const response = {
+    // crea una respuesta con información sobre los libros
+    service: "books",
+    architecture: "microservices",
+    length: data.dataLibrary.books.length,
+    data: data.dataLibrary.books,
+  };
+  logger("Get book data"); // registra un mensaje en los registros
+  return res.send(response); // devuelve la respuesta al cliente
+});
+
+// define un controlador para la ruta "/title/:title"
+router.get("/title/:title", (req, res) => {
+  // busca los libros que contengan el título buscado
+  const titles = data.dataLibrary.books.filter((title) => {
+    return title.title.includes(req.params.title);
+  });
+  // crea una respuesta con información sobre los libros que coinciden con el título buscado
+  const response = {
+    service: "books",
+    architecture: "microservices",
+    length: titles.length,
+    data: titles,
+  };
+  return res.send(response); // devuelve la respuesta al cliente
+});
+
+// creamos una ruta para la busqueda por id del autor del libro 
+router.get("/author/:authorid", (req, res) => {
+  //crear una variable async que espera los datos del endpoint 
+  //el cual hace un fetch al servidor donde estan los nombres de los autores de cada libro
+  const GetAuthorById = async (req, res) => {
+    let EndPoint = "http://authors:3000/api/v2/authors/" + req.params.authorid;
+    let AutoresById = await fetch(EndPoint);
+    const AuthorJson = await AutoresById.json();
+    const book = data.dataLibrary.books.filter((Book) => {
+      return Book.authorid == AuthorJson.data[0].id
+    });
+
+    // Creamos un objeto de respuesta con los datos de los libros buscado por autorid
+    const response = {
+      service: "Get books by author id",
+      architecture: "microservices",
+      length: book.length,
+      author: AuthorJson.data[0].author,
+      Country: AuthorJson.data[0].country,
+      data: book,
+    };
+
+    // Enviamos la respuesta
+    return res.send(response);
+  }
+  GetAuthorById(req, res);
+});
+
+
+//obtener los libro buscando por nombre de autor
+router.get("/authorName/:author", (req, res) => {
+  const GetAuthorByName = async (req, res) => {
+    let EndPoint = "http://authors:3000/api/v2/authors/author/" + req.params.author;
+    let AutoresByName = await fetch(EndPoint);
+    const AuthorJson = await AutoresByName.json();
+    const book = data.dataLibrary.books.filter((Book) => {
+      return Book.authorid == AuthorJson.data[0].id
+    });
+
+    // Creamos un objeto de respuesta con los datos de los libros buscado por autorid
+    const response = {
+      service: "Get books by Author name ",
+      architecture: "microservices",
+      length: book.length,
+      author: AuthorJson.data[0].author,
+      Country: AuthorJson.data[0].country,
+      data: book.titles,
+    };
+
+    // Enviamos la respuesta
+    return res.send(response);
+  }
+  GetAuthorByName(req, res);
+});
+
+
+// crea una ruta que pedira dos parametros de fecha para buscar los libros publicados entre las dos fechas
+router.get('/year/:startYear/:endYear', (req, res) => {
+  // Parseamos a enteros los parametros de la ruta y se almacena en variables de inicio y fin.
+  const startYear = parseInt(req.params.startYear);
+  const endYear = parseInt(req.params.endYear);
+  // Se usa filter para que solo devuelva los libros que cumplan la condicion.
+  const yearBooks = data.dataLibrary.books.filter((book) => {
+    return book.year >= startYear && book.year <= endYear;
+  });
+
+  const response = {
+    service: 'books',
+    architecture: 'microservices',
+    length: yearBooks.length,
+    data: yearBooks,
+  };
+
+  return res.send(response);
+});
+
+// para filtrar por años mayor que la fecha escita en la ruta
+router.get('/yearOlder/:year', (req, res) => {
+  const years = parseInt(req.params.year);
+  const yearBooks = data.dataLibrary.books.filter((year) => {
+    return year.year >= years;
+  });
+  const response = {
+    service: 'books',
+    architecture: 'microservices',
+    length: yearBooks.length,
+    data: yearBooks,
+  };
+  return res.send(response);
+});
+
+//para filtrar por años menor que la fecha escita en la ruta
+router.get('/yearLess/:year', (req, res) => {
+  const years = parseInt(req.params.year);
+  const yearBooks = data.dataLibrary.books.filter((year) => {
+    return year.year <= years;
+  });
+  const response = {
+    service: 'books',
+    architecture: 'microservices',
+    length: yearBooks.length,
+    data: yearBooks,
+  };
+  return res.send(response);
+});
+
+// para filtrar por años igaul al escito en la ruta
+router.get('/thisYear/:year', (req, res) => {
+  const years = parseInt(req.params.year);
+  const yearBooks = data.dataLibrary.books.filter((year) => {
+    return year.year === years;
+  });
+  const response = {
+    service: 'books',
+    architecture: 'microservices',
+    length: yearBooks.length,
+    data: yearBooks,
+  };
+  return res.send(response);
+});
+
+// define un controlador para la ruta "/title/:title"
+router.get("/Distributed/:country", (req, res) => {
+  let countries = [];
+  for (let country of data.dataLibrary.books) {
+     let contienePais = country.distributedCountries.filter((pais)=>{
+      return pais == req.params.country
+    });
+    if(contienePais.length > 0){
+      countries.push(country);
+    }
+  }
+  if (countries.length == 0) {
+    return res.status(404).send("No se encontró el ningun país de distribucion");
+  }
+  // crea una respuesta con información sobre los paises donde se distribuye ese libro
+  const response = {
+    service: "books",
+    architecture: "microservices",
+    length: countries.length,
+    data: countries,
+    
+  };
+  return res.send(response); // devuelve la respuesta al cliente
+});
+
+module.exports = router;
+
+/*
+Este código es un ejemplo de cómo crear una API de servicios utilizando Express y un enrutador. El enrutador define dos rutas: una para obtener todos los libros y otra para obtener libros por título. También utiliza una función simple de registro para registrar mensajes en los registros.
+*/
---
+++ microservices/countries-service/routes/locations/index.js
@@ -0,0 +1,101 @@
+// Importamos la biblioteca Express
+const express = require("express");
+
+// Importamos el archivo data-library.js que contiene la información sobre los países.
+const data = require("../../data/data-library");
+
+// Creamos un router de Express
+const router = express.Router();
+
+// Creamos una función de registro que imprime mensajes de registro en la consola
+const logger = (message) => console.log(`Countries Service: ${message}`);
+
+// Creamos una ruta GET en la raíz del router que devuelve todos los países
+router.get("/", (req, res) => {
+  // Creamos un objeto de respuesta con información sobre el servicio y los datos de los países
+  const response = {
+    service: "countries",
+    architecture: "microservices",
+    length: data.dataLibrary.countries.length,
+    data: data.dataLibrary.countries,
+  };
+  // Registramos un mensaje en la consola
+  logger("Get countries data");
+  // Enviamos la respuesta al cliente
+  return res.send(response);
+});
+
+router.get("/country/:capital", (req, res) => {
+  // buscar el país que contenga la capital que se busca
+  let country = null;
+  for (let countryCode in data.dataLibrary.countries) {
+    const currentCountry = data.dataLibrary.countries[countryCode];
+    if (currentCountry.capital == req.params.capital) {
+      country = currentCountry;
+    }
+    break;
+  }
+  // si no se encuentra el país, devolver un mensaje de error
+  if (!country) {
+    return res.status(404).send("No se encontró el país con la capital buscada");
+  }
+  // si se encuentra el país, crear una respuesta con el nombre del país
+  const response = {
+    service: "País por capital",
+    architecture: "microservicios",
+    data: country.name,
+  };
+  return res.send(response); // devuelve la respuesta al cliente
+});
+
+// Listar los nombres de los países y los autores según la capital del país que se busca
+router.get("/AuthorCountry/:capital", async (req, res) => {
+  // buscar el país que contenga la capital que se busca
+  let country = null;
+  for (let countryCode in data.dataLibrary.countries) {
+    const currentCountry = data.dataLibrary.countries[countryCode];
+    if (currentCountry.capital == req.params.capital) {
+      country = currentCountry;
+      break;
+    }
+  }
+  // si no se encuentra el país, devolver un mensaje de error
+  if (!country) {
+    return res.status(404).json({
+      error: "No se encontró el país con la capital buscada"
+    });
+  }
+
+  // obtener los nombres de autores que pertenecen al país buscado
+  let authorsResponse = await fetch(`http://authors:3000/api/v2/authors/CountryAuthor/${country.name}`);
+  const authorJson = await authorsResponse.json();
+  const authorNames = [];
+  for(let author of authorJson.data){
+    authorNames.push(author.author);
+  }
+
+  // obtener los títulos de libros que se han distribuido en el país buscado
+  const booksResponse = await fetch(`http://books:4000/api/v2/books/Distributed/${country.name}`);
+  const booksData = await booksResponse.json();
+  const bookTitles = [];
+  for(let book of booksData.data){
+    bookTitles.push(book.title);
+  }
+
+  // crear la respuesta que se enviará al cliente
+  const response = {
+    service: "Autores, libros y pais por capital",
+    architecture: "microservicios",
+    authores: authorNames.length,
+    autores: authorNames,
+    books: bookTitles.length,
+    Titles: bookTitles,
+    data: country
+  };
+  return res.send(response); // devuelve la respuesta al cliente
+});
+
+
+
+// Exportamos el router
+module.exports = router;
